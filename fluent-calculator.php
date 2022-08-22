1c6dbf01f63d02351062ddbac2699ee747d9148f
cupid chakma <greendaycu20@gmail.com>
2022-08-22 22:48:01 +0600

    updated final solution

--- fluent-calculator.php
+++ fluent-calculator.php
@@ -1,8 +1,6 @@
-<?php
-
-
+<?php 
 class FluentCalculator{
-    
+  
     public $values                 = array('zero' => 0, 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9, 'zero' => 0);
     public $operation              = array('plus'=> '+', 'minus' => '-', 'times' => '*', 'dividedBy' => '/');
     public $value_stack            = array();
@@ -11,17 +9,18 @@ class FluentCalculator{
     public $expression_string;
     public $number_string;
 
-    
     public static function init() {
-        return new FluentCalculator();
+      return new FluentCalculator();
     }
-    
+  
     public function __get($name)
     {
         if( isset( $this->values[$name] ) && is_int( $this->values[$name] ) ) {
             array_push($this->value_stack, $this->values[$name]);
         } elseif( isset( $this->operation[$name] ) && is_string( $this->operation[$name] )) {
             array_push( $this->value_stack, $this->operation[$name] );
+        } else {
+           throw new InvalidInputException();
         }
 
         return $this;
@@ -33,6 +32,8 @@ class FluentCalculator{
             array_push($this->value_stack, $this->values[$name]);
         } elseif( isset( $this->operation[$name] ) && is_string( $this->operation[$name] ) ) {
             array_push( $this->value_stack, $this->operation[$name] );
+        }else {
+           throw new InvalidInputException();
         }
 
         // Group Numbers & Get Last Sign If Multiple Signs Exists Between Two Numbers Gap
@@ -69,14 +70,7 @@ class FluentCalculator{
                 }
             }
         }
-
-        /**
-         * If the expression stack contains sign(string) at the last element remove it
-         */
-        if( is_string( $this->expression_stack[count($this->expression_stack) - 1] ) ){
-            unset($this->expression_stack[count($this->expression_stack) - 1]);
-        }
-
+      
         /**
          * Evaluate The Following Expression
          */
@@ -85,24 +79,95 @@ class FluentCalculator{
         while( ! empty( $this->expression_stack ) ) {
             $popped_value = array_shift( $this->expression_stack );
             if( is_int( $popped_value ) ) {
+                if( strlen( $popped_value ) > 9 ) {
+                    throw new DigitCountOverflowException();
+                }
                 $evaluated_data = $popped_value;
             } else if( $popped_value === '+' ) {
-                $evaluated_data += array_shift( $this->expression_stack );
+               if( $evaluated_data == '' && is_string( $popped_value ) ) {
+                    $evaluated_data = 0;
+                }
+              
+                $popped_value    = array_shift( $this->expression_stack );
+              
+                if( $popped_value == '' && ! is_int( $popped_value ) ) {
+                    continue;
+                }
+
+                if( strlen( $popped_value ) > 9 ) {
+                    throw new DigitCountOverflowException();
+                }
+
+                $evaluated_data +=  $popped_value;
             } elseif( $popped_value === '-' ) {
-                $evaluated_data -= array_shift( $this->expression_stack );
+               if( $evaluated_data == '' && is_string( $popped_value ) ) {
+                    $evaluated_data = 0;
+                }
+              
+                $popped_value    = array_shift( $this->expression_stack );
+              
+                if( $popped_value == '' && ! is_int( $popped_value ) ) {
+                    continue;
+                }
+
+                if( strlen( $popped_value ) > 9 ) {
+                    throw new DigitCountOverflowException();
+                }
+
+                $evaluated_data -= $popped_value;
             } elseif( $popped_value === '*' ) {
-                $evaluated_data *= array_shift( $this->expression_stack );
+               if( $evaluated_data == '' && is_string( $popped_value ) ) {
+                    $evaluated_data = 0;
+                }
+              
+                $popped_value    = array_shift( $this->expression_stack );
+              
+                if( $popped_value == '' && ! is_int( $popped_value ) ) {
+                    continue;
+                }
+
+                if( strlen( $popped_value ) > 9 ) {
+                    throw new DigitCountOverflowException();
+                }
+
+                $evaluated_data *= $popped_value;
             } else {
-                $evaluated_data /= array_shift( $this->expression_stack );
+               if( $evaluated_data == '' && is_string( $popped_value ) ) {
+                    $evaluated_data = 0;
+                } 
+              
+                $popped_value = array_shift( $this->expression_stack );
+              
+                if( $popped_value == '' && ! is_int( $popped_value ) ) {
+                    continue;
+                }
+
+                if( strlen( $popped_value ) > 9 ) {
+                    throw new DigitCountOverflowException();
+                }
+
+                if( $popped_value === 0 ) {
+                  throw new DivisionByZeroException();
+                }
+
+                $evaluated_data /= $popped_value;
                 if( is_float( $evaluated_data ) ) {
                     $evaluated_data = (int) $evaluated_data;
                 }
             }
+            
+            
+            if( strlen( $evaluated_data ) > 9 ) {
+              $evaluated_data_2 = abs( $evaluated_data );
+               if( strlen( $evaluated_data_2 ) > 9 ) {
+                    throw new DigitCountOverflowException();
+               } else {
+                    continue;
+               }
+            }
         }
-
-        return $evaluated_data;
+        return empty( $evaluated_data ) ? 0 : $evaluated_data;
         
     }
 }
-
-echo FluentCalculator::init()->two->plus->five->zero->two->eight->six->dividedBy->dividedBy->five->two->minus->plus->four->four->one->seven->nine->four->zero->three->eight->three->one->three->nine->four->one->zero->nine->two();
+FluentCalculator::init();
